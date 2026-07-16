34233e5e927adafd1a066e85c4e6e68c4e54ba4f
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-07-16 15:40:40 +0545

    day 4 code

---
+++ day4.py
@@ -0,0 +1,4 @@
+# day 4 comments escape sequence cahracter and print statement
+print("hello how are youu\n today is very special day")
+print("hello world ")     # This prints Hello, World! 
+print("hey" ,6 ,7 ,8 )
