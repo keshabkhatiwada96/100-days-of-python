3b100c3866c52cc49ff9dc7e69f67b44c395a26d
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-07-21 17:32:42 +0545

    taking user input

---
+++ day8.py
@@ -0,0 +1,12 @@
+# Taking User Input in Python
+
+a = input("enter your name : ")
+print (a)
+
+
+x = input("enter first number : ")
+y = input("enter seconf number : ")
+
+print(x + y) #withount int function answer will be in concatinated form.
+
+print(float(x) + float(y)) #with int function answer will be sum of two number.
