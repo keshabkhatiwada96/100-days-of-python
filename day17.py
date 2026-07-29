8bc060981c82f41f7079ea013d2ca085193360cc
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-07-29 17:30:58 +0545

    break and continue

---
+++ day17.py
@@ -0,0 +1,21 @@
+# break and continue statement
+
+
+# break statement
+for r in range (1 , 20):
+   
+    if r == 17:
+        break    #since i have used (break) when the value of r is 17 the code stops.
+    print(r)
+
+print("+++++++++++++++++++++++++++++++++")
+
+# continue statement
+
+for r in range(1, 20):
+
+    if r == 17:
+        continue    #since i have used (continue) when the value of r is 17 the code skips r and continue.
+    print(r)
+
+print("+++++++++++++++++++++++++++++++++")
