0b4c41aa6fcbbb8aa0239776135ff9e58549ebf1
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-07-30 19:01:54 +0545

    day18 Function in Python

---
+++ day18.py
@@ -0,0 +1,29 @@
+# Functions in Python
+
+# Function to check which number is greater
+def isgreater(a, b):      #def is example of user defined function
+    if a>b:
+        print("First Number is Greater")
+    else:
+        print("Second Number is greater or Number is equal")
+# Calling the same function with different values
+isgreater(10,20)
+
+isgreater(50,30)
+
+isgreater(5,5)
+
+
+# Creating an empty function for future use
+def islesser(a,b):
+    pass
+
+
+# Function using string
+def hello(n):
+    print("welcome",n)
+
+# Passing different string values to the function
+hello(" keshab ")
+hello(" khatiwada ")
+hello(" BIT ")
