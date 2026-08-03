3c18b88d0c9170334453d70ce1bf5525bc1eacf5
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-08-03 21:59:02 +0545

    random OTP generator

--- otpgenerator.py
+++ otpgenerator.py
@@ -1,5 +1,24 @@
+#  Random 4-Digit OTP Generator and Verification
+# This program generates a random 4-digit One-Time Password (OTP). 
+# After the OTP is generated, the user is prompted to enter it. 
+# If the entered OTP matches the generated OTP, the program displays a **"Login Successful"** message.
+#  Otherwise, it displays **"Wrong OTP"** and the program terminates.
+
 import random
-input("press ENTER to generate a random OTP...")
+
+input("press ENTER to generate a random OTP.. ")
 OTP = random.randint(1000,9999)
 print("\nYour OTP:\n",OTP)
 
+user_otp=int(input("enter your OTP: "))
+if user_otp== OTP:
+ print("Login Sucessful")
+ print("""\
+ "🎉 **Congratulations!** 🎉
+
+You have successfully logged in.
+
+Your OTP has been verified successfully. Welcome! We hope you have a great experience using the system.
+""")
+else:
+ print("wrong OTP")
