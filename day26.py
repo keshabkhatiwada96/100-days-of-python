eebc8bbbc0c9f6aea9177963e58347430ed39469
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-08-07 20:19:28 +0545

    docstring in python

---
+++ day26.py
@@ -0,0 +1,34 @@
+# Docstrings in Python
+def greet():
+    """This function prints a greeting message."""
+    print("Hello!")
+
+greet()
+print(greet.__doc__)
+
+# Comment vs Docstring
+# Comment:
+# Starts with #
+# Ignored by Python
+# Used for writing notes for programmers
+
+# Docstring:
+# Uses """ """ or ''' '''
+# Stored by Python
+# Used to describe functions, classes, and modules
+
+# PEP 8 (Python Enhancement Proposal 8) is the official style guide for writing Python code.
+
+# It gives rules and recommendations for making Python code:
+#easier to read
+#consistent
+#clean
+#professional
+
+print("********************")
+# The Zen of Python
+import this
+
+# The Zen of Python is a collection of principles
+# written by Tim Peters that describes Python's philosophy
+# and encourages writing simple, readable, and beautiful code.
