677ab5939760cab4068c20b97d7cc75265c53d65
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-08-13 20:55:33 +0545

    Exception handeling in python

---
+++ day32.py
@@ -0,0 +1,12 @@
+#Exception Handling in Python
+
+a = input("enter a number: ")
+print(f"Multiplicaton table of {a} is: ")
+try:                        # try Runs the code that may cause an error
+                            
+    for i in range(1,11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
+except:                      #if error occur the except block will be executed
+    print("Invalid Input")
+print("Important lines of code ")
+print("End of Program")
