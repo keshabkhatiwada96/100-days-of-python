b6e8b2444283c14351386bf574ce9469b1e5d82a
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-08-15 14:31:13 +0545

    random password generator

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -10,15 +10,15 @@ def generate_password():
         try:
             length = int(input("Enter password length: "))
             if length < 1:
-                print("Length must be at least 1. Try again.")
+                print("Length must be at least 1.")
                 continue
             break
         except ValueError:
-            print("Please enter a valid whole number.")
+            print("Please enter a valid number.")
             
     # Generate the secure password
     password = ''.join(secrets.choice(alphabet) for _ in range(length))
     return password
 
 # Run the generator
-print("Your secure password is:", generate_password())
+print("Your secure password is: ", generate_password())
