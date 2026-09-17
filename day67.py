af92418cf19bd86fb7c18dc475d3120a08a5d1d0
Keshab Khatiwada <khatiwadakeshab168@gmail.com>
2026-09-17 18:02:25 +0545

    Linieer regresion with train test model

---
+++ day67.py
@@ -0,0 +1,32 @@
+# Linieer regresion with train test model
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+temperature_in_C = [[24], [25], [28], [26], [27], [30], [29], [31], [32], [33]]
+ice_cream_sales = [40, 45, 55, 50, 53, 31, 56, 60, 62, 68]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    temperature_in_C,
+    ice_cream_sales,
+    test_size=0.3,
+    random_state=42
+)
+
+print("Training data:", X_train)
+print("Testing data:", X_test)
+print("Training sales:", y_train)
+print("Testing sales:", y_test)
+
+# creating linerregression model
+model = LinearRegression()
+print("liner regression model created")
+
+# traing model
+model.fit(X_train,y_train)
+print("model trained")
+
+# predctions
+predctions = model.predict(X_test)
+
+print("Actual sale:", y_test) 
+print("Predicted sale:", predctions)
